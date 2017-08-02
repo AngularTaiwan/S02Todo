f5e7a421c70bbd04c04287856a7b0a17fda3302c
PoyChang <poypost@gmail.com>
2017-08-02 20:56:50 +0800

    implement localstroage

--- todomvc-angular-cli/src/app/app.component.ts
+++ todomvc-angular-cli/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Todo, TodoStatus } from './models';
 import { generateId } from './mock';
@@ -8,41 +8,72 @@ import { generateId } from './mock';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  filterCondition: TodoStatus | undefined;
+  filterCondition: TodoStatus|undefined;
 
-  todoList: Todo[] = [
-    new Todo({
-      id: generateId(),
-      name: 'Taste JavaScript',
-      status: TodoStatus.Completed,
-    }),
-    new Todo({
-      id: generateId(),
-      name: 'Buy a unicorn',
-      status: TodoStatus.Active,
-    }),
-  ];
+  todoList: Todo[] = [];
+
+  ngOnInit(): void {
+    const data = JSON.parse(localStorage.getItem('todoList'));
+    this.todoList = data.map(p => new Todo({id: p.id, name: p.name, status: p.status}));
+  }
+
+  saveToLocalStorage() {
+    localStorage.setItem('todoList', JSON.stringify(this.todoList));
+  }
 
   createNewTodo(input: HTMLInputElement) {
     this.todoList.push(new Todo({
-      id: generateId(),
+      id: Math.max(0, ...this.todoList.map(p => p.id)) + 1,
       name: input.value,
       status: TodoStatus.Active,
     }));
     input.value = '';
+    this.saveToLocalStorage();
+  }
+
+  updateTodo(todo: Todo, input: HTMLInputElement) {
+    todo.name = input.value;
+    todo.selected = false;
+    this.saveToLocalStorage();
+  }
+
+  switchStatus(todo: Todo) {
+    todo.switchStatus();
+    this.saveToLocalStorage();
+  }
+
+  deleteTodo(index: number) {
+    this.todoList.splice(index, 1);
+    this.saveToLocalStorage();
+  }
+
+  clearCompletedTodo() {
+    this.todoList = this.todoList.filter(todo => !todo.isCompleted);
+    this.saveToLocalStorage();
+  }
+
+  completedAllTodo() {
+    this.todoList.forEach(todo => todo.status = TodoStatus.Completed);
+    this.saveToLocalStorage();
   }
 
   // setting
-  showAll() { this.filterCondition = undefined; }
-  showActive() { this.filterCondition = TodoStatus.Active; }
-  showCompleted() { this.filterCondition = TodoStatus.Completed; }
+  show(condition: string) {
+    this.filterCondition = TodoStatus[condition];
+  }
+  // showAll() { this.filterCondition = undefined; }
+  // showActive() { this.filterCondition = TodoStatus.Active; }
+  // showCompleted() { this.filterCondition = TodoStatus.Completed; }
 
   // check
-  isShowAll() { return !this.filterCondition; }
-  isShowActive() { return this.filterCondition === TodoStatus.Active; }
-  isShowCompleted() { return this.filterCondition === TodoStatus.Completed; }
+  isShow(condition: string) {
+    return this.filterCondition === TodoStatus[condition];
+  }
+  // isShowAll() { return !this.filterCondition; }
+  // isShowActive() { return this.filterCondition === TodoStatus.Active; }
+  // isShowCompleted() { return this.filterCondition === TodoStatus.Completed; }
 
   selectTodo(todo: Todo, input: HTMLInputElement) {
     todo.selected = true;
@@ -50,21 +81,10 @@ export class AppComponent {
     setTimeout(() => input.focus(), 0);
   }
 
-  deleteTodo(index: number) {
-    this.todoList.splice(index, 1);
-  }
-
-  clearCompletedTodo() {
-    this.todoList = this.todoList.filter(todo => !todo.isCompleted);
-  }
-
   get leftTodo() {
     return this.todoList.filter(todo => !todo.isCompleted).length;
   }
 
-  completedAllTodo() {
-    this.todoList.forEach(todo => todo.status = TodoStatus.Completed);
-  }
 
 
 }
